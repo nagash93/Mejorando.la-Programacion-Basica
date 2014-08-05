6f967746321f9f50076be79af793650e3dda0ea7
Pablo Ángel Trinidad Paz <pabloatp0204@gmail.com>
2014-08-04 19:37:51 -0500

    Alerts antes de aplicacion

--- Clase 1/codigo.js
+++ Clase 1/codigo.js
@@ -2,3 +2,18 @@
 alert("Hola mamá, ya sé programar!!!");
 	// formato funcion(PARAMETROS);
 // Genera una ventana emergente con el mensaje dentro de las comillas
+
+//Declaración de variables de la forma variable = valor
+var nombre = "Pablo Angel";				// <--- Tipo: Texto
+var apellido = "Trinidad Paz";			// <--- Tipo: Texto
+var edad = 17;							// <--- Tipo: Número
+//Declaración de variables de la forma variable = valor
+
+// Ventana emergente con las variables
+alert(nombre + " " + apellido + "\ntiene " + edad + " años");
+	// \n da un salto de línea
+// Ventana emergente con las variables
+
+// Ventana emergente que ejecuta operaciones y concatenaciones
+alert("2" + 5 * 8)
+// Ventana emergente que ejecuta operaciones y concatenaciones
