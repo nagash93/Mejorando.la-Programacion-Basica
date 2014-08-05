085b6afb89acdfd77f33069c77517a098082fec2
Pablo Ángel Trinidad Paz <pabloatp0204@gmail.com>
2014-08-04 19:49:31 -0500

    .

--- Clase 1/codigo.js
+++ Clase 1/codigo.js
@@ -12,13 +12,13 @@ peso = prompt("¿Cuál es tu peso en KG?", "70");
 alert("¿Quieres saber cuál es tu peso en Marte?")
 // Ventana emergente con mensaje
 
-// Convertimos el peso en peso en Marte con la siguiente ecuación
-pesoEnMarte = (peso / 9.81) * 3.711
-// Convertimos el peso en peso en Marte con la siguiente ecuación
-
 // Convertimos peso a número con parseInt() pero también se puede hacer con Number()
 peso = parseInt(peso);
 // Convertimos peso a número con parseInt() pero también se puede hacer con Number()
+
+// Convertimos el peso en peso en Marte con la siguiente ecuación
+pesoEnMarte = (peso / 9.81) * 3.711
+// Convertimos el peso en peso en Marte con la siguiente ecuación
 
 // Imprimimos valor dado por el usuario
 alert(peso + 10);
